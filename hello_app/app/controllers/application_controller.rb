fc89c39e8225bbddadf31bd31ead3560365c497d
Kelsi Olstad <kelsi@popcodes.com>
2022-10-14 09:29:13 -0600

    define hello action

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    # adding an action called hello to application controller
+    def hello
+        render html: "Hello, world!"
+    end
 end
