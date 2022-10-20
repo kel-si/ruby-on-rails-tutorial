20300d59b625aee60328c107472cd8d335a9499e
Kelsi Olstad <kelsi@popcodes.com>
2022-10-20 13:34:44 -0600

    add comments

--- sample_app/db/migrate/20221020191209_create_users.rb
+++ sample_app/db/migrate/20221020191209_create_users.rb
@@ -1,9 +1,14 @@
+# migrate using db:migrate
+
 class CreateUsers < ActiveRecord::Migration[7.0]
+  # method that determines the changes to be made to the database
   def change
+    # rails method to create a table in the db with columns specified in the block
     create_table :users do |t|
       t.string :name
       t.string :email
 
+      # creates two columns - created_at and updated_at
       t.timestamps
     end
   end
