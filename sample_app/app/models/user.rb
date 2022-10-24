d527e9449c5ed8e0de897713cda3765f783ff2ad
Kelsi Olstad <kelsi@popcodes.com>
2022-10-24 09:49:59 -0600

    create password_digest column in db

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
     # callback to downcase email before being saved to db
-    before_save { email.downcase! }
+    # another option is { email.downcase! }
+    before_save { self.email = email.downcase }
 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+
+    # adds ability to save hashed password to db
+    # pair of virtual attritbutes (password and password_confirmation) including presence validation upon object creation as well as validation for matching
+    # authenticate method that returns user when password is correct (else returns false)
+    # has_secure_password
 end
