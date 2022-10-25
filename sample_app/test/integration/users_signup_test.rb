ce128d361698d1f68bf3d02c750ec77b4c702844
Kelsi Olstad <kelsi@popcodes.com>
2022-10-25 14:42:11 -0600

    write test for invalid information on signup

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    # technically unnecessary but adds conceptual clarity and to double-check
+    get signup_path
+    # check User.count before and after signup to make sure an invalid user was not created
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    # verify correct status returned
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+  end
+end
