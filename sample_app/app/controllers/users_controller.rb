05c44d721032b3b86682c23c04f61dfdccd99466
Kelsi Olstad <kelsi@popcodes.com>
2022-10-25 11:45:18 -0600

    add create action for user registration

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user]) # not the final version (bad)
+    if @user.save
+    else
+      # corresponds to HTTP status 422 Unprocessable Entry
+      render 'new', status: :unprocessable_entry
+    end
+  end
 end
