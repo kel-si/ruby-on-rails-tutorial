50250eec15844b97f779a19767c7a3dcb0027580
Kelsi Olstad <kelsi@popcodes.com>
2022-10-25 16:39:39 -0600

    add flash message for successful signup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!🦋"
       # @user here is equivalent to user_url(@user)
       redirect_to @user
     else
