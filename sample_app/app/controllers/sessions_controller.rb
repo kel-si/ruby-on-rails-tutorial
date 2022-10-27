d0c72479537a57e5cdbb67b9d71c28d54084e3a5
Kelsi Olstad <kelsi@popcodes.com>
2022-10-27 16:13:58 -0600

    clear session prior to login

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -4,8 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # log in user
+    if user && user.authenticate(params[:session][:password])+
+      # clear session to prevent session fixation prior to log in
+      reset_session
+      log_in user
+      redirect_to user
     else
       # create error message
       flash.now[:danger] = "Invalid email/password combination🤚"
