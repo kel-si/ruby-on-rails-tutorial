4c19b2054d70f199d25a7da7a7ca42db5985d295
Kelsi Olstad <kelsi@popcodes.com>
2022-10-31 08:44:47 -0600

    add comments

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -1,8 +1,11 @@
 module SessionsHelper
+    # logs given user in
+    # user is defined in sessions_controller (create action)
     def log_in(user)
         session[:user_id] = user.id
     end
 
+    # returns current logged-in user (if any)
     def current_user
         if session[:user_id]
             @current_user ||= User.find_by(id: session[:user_id])
