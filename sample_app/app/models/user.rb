3aa3e57ea51f7ad53fb89ab16adeb943daa6f634
Kelsi Olstad <kelsi@popcodes.com>
2022-10-31 16:31:18 -0600

    fix typo

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -31,6 +31,6 @@ class User < ApplicationRecord
 
     def remember
         self.remember_token = User.new_token
-        update_attributes(:remember_digest, User.digest(remember_token))
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
