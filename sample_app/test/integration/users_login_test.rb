571493f64e49d6f46c22fb66bd042601af205446
Kelsi Olstad <kelsi@popcodes.com>
2022-11-01 12:51:40 -0600

    write tests for remember me checkbox

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -76,3 +76,18 @@ class LogoutTest < Logout
         assert_redirected_to root_url
     end
 end
+
+class RememberingTest < UsersLogin
+
+    test "login with remembering" do
+        # box checked
+        log_in_as(@user, remember_me: '1')
+        assert_not cookies[:remember_token].blank?
+    end
+
+    test "login without remembering" do
+        # log in to set the cookie (to make sure it is forgotten)
+        log_in_as(@user, remember_me: '0')
+        assert cookies[:remember_token].blank?
+    end
+end
