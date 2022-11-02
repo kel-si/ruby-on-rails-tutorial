f1b151aa9eb046b521d21466201b9fd721a6c8d8
Kelsi Olstad <kelsi@popcodes.com>
2022-11-02 13:26:37 -0600

    generate migration for admin column

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -9,10 +9,12 @@
 
 # Create a main sample user:
 
-User.create!(name:  "Example User",
+User.create!(
+    name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
-    password_confirmation: "foobar")
+    password_confirmation: "foobar",
+    admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
