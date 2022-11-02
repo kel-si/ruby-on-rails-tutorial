165d347f3f0fffe6bfc3d5aa9c295032e6f0a7b7
Kelsi Olstad <kelsi@popcodes.com>
2022-11-02 11:15:51 -0600

    add index action to display all users and link in header

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   # arrange for a particular method to be called before actions
   # by default, before filters apply to all controller actions
   # here we have limited filter to act on only edit and update
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
