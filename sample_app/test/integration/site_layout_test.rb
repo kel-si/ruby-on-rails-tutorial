4087c2bbe7e746f30bee63de7716fc7cc77079dc
Kelsi Olstad <kelsi@popcodes.com>
2022-11-02 11:36:07 -0600

    test for logged-in / logged-out users accessing /users

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:frosty)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +17,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_equal "Sign Up | Ruby on Rails Tutorial Sample App", full_title("Sign Up")
+    log_in_as(@user)
+    assert users_path
+    delete logout_path
+    assert_select "a[href=?]", users_path, count: 0
   end
 end
