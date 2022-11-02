105c94c75c10582e23dc69dfc581d4c81a036893
Kelsi Olstad <kelsi@popcodes.com>
2022-11-02 14:19:16 -0600

    restrict destroy action to admins

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   # arrange for a particular method to be called before actions
   # by default, before filters apply to all controller actions
   # here we have limited filter to act on only edit and update
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -48,6 +49,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
   private
 
       def user_params
@@ -70,5 +77,10 @@ class UsersController < ApplicationController
       def correct_user
         @user = User.find(params[:id])
         redirect_to(root_url, status: :see_other) unless current_user?(@user)
+      end
+
+      # confirms an admin user
+      def admin_user
+        redirect_to(root_url, status: :see_other) unless current_user.admin?
       end
 end
