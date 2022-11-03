9f038cc7394039f69fe8d01376b8227cbe718893
Kelsi Olstad <kelsi@popcodes.com>
2022-11-03 15:30:48 -0600

    refactor; test activated/inactive behaviour

--- sample_app/test/integration/users_index_test.rb
+++ sample_app/test/integration/users_index_test.rb
@@ -1,34 +1,61 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
 
   def setup
     @admin = users(:frosty)
     @non_admin = users(:daisy)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+
+  test "should render the index page" do
     assert_template 'users/index'
+  end
+
+  test "should paginate users" do
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    # checking the delete link is there for nonadmin users
+  end
+
+  test "should have delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name 
+      assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
-    # test deleting one user
-    assert_difference 'User.count', -1 do
-      delete user_path(@non_admin)
-      assert_response :see_other
-      assert_redirected_to users_url
-    end
   end
 
-  test "index as a non-admin" do
+  test "should be able to delete non-admin user" do
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+    assert_response :see_other
+    assert_redirected_to users_url
+  end
+
+  test "should display only activated users" do
+    User.paginate(page: 1).first.toggle!(:activated)
+    assigns(:users).each do |user|
+      assert user.activated
+    end
+  end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
+
+  test "should not delete links as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
