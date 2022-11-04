022f357ded1d46ecddc2f70d5b6a7911a8d92dd4
Kelsi Olstad <kelsi@popcodes.com>
2022-11-04 15:06:12 -0600

    update action and check expiration on password reset link

--- sample_app/app/controllers/password_resets_controller.rb
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
   def new
   end
 
@@ -21,15 +22,46 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      # forget any user sessions upon password reset to prevent session hijacking
+      @user.forget
+      reset_session
+      log_in(@user)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # Before filters:
+  
   def get_user
+    # passed email in through form
     @user = User.find_by(email: params[:email])
   end
 
   def valid_user
     unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    # delegates to User model
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset link expired."
+      redirect_to new_password_reset_url
     end
   end
 end
