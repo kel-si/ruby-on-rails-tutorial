9fc419f6ec8df4c232684c6f2279208a3377d66d
Kelsi Olstad <kelsi@popcodes.com>
2022-11-04 11:26:23 -0600

    reset digest to db

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # creates an accessible attribute (for storage in the cookies but not in the database)
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     before_save :downcase_email
     before_create :create_activation_digest
@@ -68,6 +68,17 @@ class User < ApplicationRecord
 
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # sets the password reset attribute
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
     end
 
     private
