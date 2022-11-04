edc6f269f2a6a37b819c6fa904dd8f58fdee51cf
Kelsi Olstad <kelsi@popcodes.com>
2022-11-04 08:21:21 -0600

    update test to include assert_response

--- sample_app/test/integration/user_show_test.rb
+++ sample_app/test/integration/user_show_test.rb
@@ -9,11 +9,13 @@ class UsersShowTest < ActionDispatch::IntegrationTest
 
   test "should redirect when user not activated" do
     get user_path(@inactive_user)
+    assert_response :redirect
     assert_redirected_to root_path
   end
 
   test "should display user when activated" do
     get user_path(@activated_user)
+    assert_response :ok
     assert_template 'users/show'
   end
 end
