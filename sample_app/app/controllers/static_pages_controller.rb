13cbc78ca9d43609e659a2ff2e31f923cce6c694
Kelsi Olstad <kelsi@popcodes.com>
2022-11-07 14:20:23 -0700

    add content to create method in Microposts controller, update homepage based on login status, refactor partials/forms

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 # StaticPagesController inherits from ApplicationController
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
