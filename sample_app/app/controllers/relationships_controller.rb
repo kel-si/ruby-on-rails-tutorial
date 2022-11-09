64ffcb923b1a0c88c0a9f87c9e690f01039a3c51
Kelsi Olstad <kelsi@popcodes.com>
2022-11-09 11:43:41 -0700

    write create and destroy actions to follow/unfollow users

--- sample_app/app/controllers/relationships_controller.rb
+++ sample_app/app/controllers/relationships_controller.rb
@@ -2,8 +2,17 @@ class RelationshipsController < ApplicationController
     before_action :logged_in_user
 
     def create
+        # getting the id of the user to follow
+        user = User.find(params[:followed_id])
+        # from cookies
+        current_user.follow(user)
+        redirect_to user
     end
 
     def destroy
+        # finding user who was followed by current user by looking for relationship id
+        user = Relationship.find(params[:id]).followed
+        current_user.unfollow(user)
+        redirect_to user, status: :see_other
     end
 end
