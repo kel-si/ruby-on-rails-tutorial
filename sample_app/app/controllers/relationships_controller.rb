cef97c78348d019179a7f87624734303e318110a
Kelsi Olstad <kelsi@popcodes.com>
2022-11-09 12:25:09 -0700

    turbo-stream requests in Relationships controller

--- sample_app/app/controllers/relationships_controller.rb
+++ sample_app/app/controllers/relationships_controller.rb
@@ -3,16 +3,22 @@ class RelationshipsController < ApplicationController
 
     def create
         # getting the id of the user to follow
-        user = User.find(params[:followed_id])
+        @user = User.find(params[:followed_id])
         # from cookies
-        current_user.follow(user)
-        redirect_to user
+        current_user.follow(@user)
+        respond_to do |format|
+            format.html { redirect_to @user }
+            format.turbo_stream
+        end
     end
 
     def destroy
         # finding user who was followed by current user by looking for relationship id
-        user = Relationship.find(params[:id]).followed
-        current_user.unfollow(user)
-        redirect_to user, status: :see_other
+       @user = Relationship.find(params[:id]).followed
+        current_user.unfollow(@user)
+        respond_to do |format|
+            format.html { redirect_to @user, status: :see_other }
+            format.turbo_stream
+        end
     end
 end
